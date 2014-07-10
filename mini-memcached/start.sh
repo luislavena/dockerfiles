683ffdeeb7d327bb5ee633c8326394d8bc248942
Luis Lavena <luislavena@gmail.com>
2014-07-09 21:01:31 -0300

    mini-memcached: workaround memcached inability to handle SIGTERM
    memcached do not handle SIGTERM triggered by `docker stop`, which
    causes the container to terminate with a non-zero exit code.
    
    To workaround this, we trap signals sent to us and re-raise as
    SIGINT so memcached exits cleanly.

--- mini-memcached/start.sh
+++ mini-memcached/start.sh
@@ -3,7 +3,16 @@
 start() {
 	local MEMORY=${MEMCACHED_MEMORY:-64}
 
-	exec memcached -u nobody -m $MEMORY
+	# launch memcached as background process
+	memcached -u nobody -m $MEMORY &
+
+	# capture it's PID
+	local PID=$!
+
+	# re-raise INT/TERM signal as SIGINT to memcached
+	trap "kill -s SIGINT $PID && exit $?" SIGINT SIGTERM
+
+	wait
 }
 
 start
